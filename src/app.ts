7acd325217586c5f7fba91727510562dddf16f48
luiabdiel <luiggiabdieldev@gmail.com>
2024-01-20 12:43:55 -0300

    feat: start server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hi, node")
+import express from 'express'
+import { PORT } from '../constants/environments'
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT, async () => {
+  console.log(`🔥 Server started at http://localhost:${PORT}`)
+})
