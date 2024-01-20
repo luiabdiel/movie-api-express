68f686a3e5f1a4eaedb46b875d65cdf90dbb8da2
luiabdiel <luiggiabdieldev@gmail.com>
2024-01-20 14:16:23 -0300

    feat: API Route Test

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { PORT } from '../constants/environments'
+import router from "./router"
 
 const app = express()
 
 app.use(express.json())
+app.use("/api/", router)
 
 app.listen(PORT, async () => {
   console.log(`🔥 Server started at http://localhost:${PORT}`)
